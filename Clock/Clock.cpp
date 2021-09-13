671ec85aff9672ddfaa2ebaad0329ff474bd2bc6
Yazan Alatrach <yazan_alatrach@hotmail.com>
2021-09-12 21:33:24 -0500

    initial Commit

---
+++ Clock/Clock.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>  
+#include <iostream>
+#include "Clock.h"
+using namespace std;
+
+
+
+Clock::Clock(){
+  cout << "Created new Clock" << endl;
+  setTime(0, 0);
+}
+Clock::Clock(int hour, int minutes){
+  setTime(hour, minutes);
+}
+void Clock::setTime(int hour, int minutes){
+  hr = hour;
+  min = minutes;
+}
+void Clock::increaseTime(int minutes){
+  min += minutes;
+  while(min >= 60){
+    hr ++;
+    min -= 60;
+  }
+}
+void Clock::printStandardTime(){
+  if (hr == 0 || hr == 24){
+    if (min < 10){
+      cout << 12 << ":" << '0' << min << endl;
+    } else {
+      cout << 12 << ":" << min << endl;
+    }
+    return;
+  }
+  cout << hr%12 << ":" << min << endl;
+}
+void Clock::printMilitaryTime(){
+  cout << hr << ":" << min << endl;
+}
+
+
