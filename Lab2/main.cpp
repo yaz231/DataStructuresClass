bf7e6744a3eb5323a8db34bafb18bfe68c1269fa
Yazan Alatrach <yazan_alatrach@hotmail.com>
2021-10-04 13:50:38 -0500

    Finished Lab

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -6,36 +6,44 @@
 
 using namespace std;
 
-int main()
-{
-  // string test = "auctions.txt";
-  // printf("Length: %d\n", test.size());
-  // printf("Please enter a file name: ");
-  // string fileName;
-  // scanf("%12s", &fileName);
-  // printf("Filename: %s", fileName);
-	LinkedStack<int> link;
-  fstream fin("auctions.txt");
+int main(){
+  bool firstItem = true;
+  string itemName;
+  printf("Please enter a file name: ");
+  string fileName;
+  cin >> fileName;
+  cout << "Filename: " << fileName << endl;
+	
+  LinkedStack<int> link; //Initialize Stack
+  fstream fin(fileName);
   string line;
   while(getline(fin, line)){
-    // if (line[0] == 'V') continue;
-      int bid;
-      // printf("HERE");
-      stringstream(line) >> bid;
-      printf("%d\n", bid);
-      if (link.isEmpty()){
-        link.push(bid);
-      } else {
-        if (bid > link.peek()) link.push(bid);
-        // printf("%d\n", bid);
-      }
+    if (firstItem){
+      firstItem = false;
+      cout << line << '\n';
+      itemName = line;
+      // printf("%s",itemName);
+      continue;
+    }
+    int bid;
+
+    stringstream(line) >> bid;
+    printf("%d\n", bid);
+    if (link.isEmpty()){ //Beginning of the text file always pushed to stack
+      link.push(bid); 
+    } else {
+      if (bid > link.peek()) link.push(bid); //If top of stack is less than new line entry, push to stack
+      // printf("%d\n", bid);
+    }
   }
   // printf("DONE\n");
+  //Verify contents of stack after traversing through file.
   // while(!link.isEmpty()){
   //   printf("%d\n", link.peek());
   //   link.pop();
   // }
-  printf("Final Bid: %d\n", link.peek());
 
+  // printf("Final Bid: %d for %s\n", link.peek(), itemName);//Final Bid is the last entry pushed to stack
+  cout << "Final Bid: " << link.peek() << " for " << itemName << endl;
   return 0;
 }  // end main
