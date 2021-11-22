588bc001a47042730f7dae2427fc88467a890601
Yazan <yazan_alatrach@hotmail.com>
2021-11-21 22:27:53 -0600

    Changes to Lab4

---
+++ Lab4/main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstdio>
+#include "QueueInterface.h"
+
+using namespace std;
+
+int main() {
+    int menuNumber;
+	printMenu();
+	scanf("%d", &menuNumber); //Scanf is used to accept user input to determine menu option
+    if (menuNumber == 1) {
+        Queue<int> q(10);
+    } else if (menuNumber == 2) {
+        Queue<float> q(10);
+    } else if (menuNumber == 3) {
+        Queue<string> q(10);
+    } else 
+        return 0;
+	while(true){
+		int queueMenuNumber;
+		printQueueMenu();
+		scanf("%d", &queueMenuNumber); //Scanf is used to accept user input to determine menu option
+		//Switch Statement to Correspond to User Input
+		switch(queueMenuNumber){
+			case 1:
+				printf("Enter the item to Add:");
+                if (menuNumber == 1){
+                    int numberToAdd;
+				    scanf("%d", &numberToAdd);
+				    q.enqueue(numberToAdd);
+                } else if (menuNumber == 2) {
+                    float floatToAdd;
+				    scanf("%f", &floatToAdd);
+				    q.enqueue(floatToAdd);
+                } else if (menuNumber == 3) {
+                    string stringToAdd;
+				    cin >> stringToAdd;
+				    q.enqueue(stringToAdd);
+                }
+				break;
+
+			case 2:
+				printf("Removing first Item from the Queue:");
+                if (menuNumber == 1){
+                    int numberRemoved = 0;
+				    q.dequeue(numberRemoved);
+                    printf("Item removed: %d", numberRemoved);
+                } else if (menuNumber == 2) {
+                    float floatRemoved;
+				    q.dequeue(floatRemoved);
+                    printf("Item removed: %d", floatRemoved);
+                } else if (menuNumber == 3) {
+                    string stringRemoved;
+				    q.dequeue(stringRemoved);
+                    printf("Item removed: %s", stringRemoved);
+                }
+				break;
+
+			case 3:
+				printf("Displaying the Bag:\n");
+				displayQueue(q);
+				break;
+
+			case 4:
+				return 0;
+		}
+	}
+  return 0;
+}  // end main
+
+
+/******************************************************************************/
+/* Function:  printMenu
+/* Inputs:    NA
+/* Outputs:   NA
+/* Purpose:   This function prints out the menu listing to display for the user
+/*						to choose from
+/******************************************************************************/
+void printMenu(){
+	printf("Welcome to the Queue Testing Menu!\n");
+	printf("\n");
+	printf("Please select from the folowing menu as our menu options have changed:\n");
+	printf("Press 1 to create an INTEGER Queue\n");
+	printf("Press 2 to create a FLOAT Queue\n");
+	printf("Press 3 to create a STRING Queue\n");
+	printf("Press anything else to Exit this Program :)\n");
+	printf("Press ENTER after you've made your selection\n");
+}
+
+void printQueueMenu(){
+	printf("Welcome to the Queue Testing Menu!\n");
+	printf("\n");
+	printf("Please select from the folowing menu as our menu options have changed:\n");
+	printf("Press 1 to add to the Queue\n");
+	printf("Press 2 to remove from the Queue\n");
+	printf("Press 3 to display the Queue's current contents\n");
+	printf("Press 4 to Clear the Queue\n");
+    printf("Press 5 to Exit this Program :)\n");
+	printf("Press ENTER after you've made your selection\n");
+
+
+
